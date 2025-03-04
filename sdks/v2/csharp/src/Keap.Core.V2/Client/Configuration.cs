5d80aabbc71bccf712fa73f8561022e6a30adccb
Keap API team <api@keap.com>
2025-03-04 00:11:04 +0000

    Updating SDK 0.0.8 [skip ci]

--- sdks/v2/csharp/src/Keap.Core.V2/Client/Configuration.cs
+++ sdks/v2/csharp/src/Keap.Core.V2/Client/Configuration.cs
@@ -35,7 +35,7 @@ namespace Keap.Core.V2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.0.7";
+        public const string Version = "0.0.8";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -119,7 +119,7 @@ namespace Keap.Core.V2.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("core-service-v2-csharp/0.0.7");
+            UserAgent = WebUtility.UrlEncode("core-service-v2-csharp/0.0.8");
             BasePath = "https://api.keap.com/crm/rest";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -572,7 +572,7 @@ namespace Keap.Core.V2.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 2.70.0.784405-hf-202502271519\n";
-            report += "    SDK Package Version: 0.0.7\n";
+            report += "    SDK Package Version: 0.0.8\n";
 
             return report;
         }
