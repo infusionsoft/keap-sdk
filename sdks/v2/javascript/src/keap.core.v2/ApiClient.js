42832c717fbf5b78c4348c7049519bca10889c4f
Keap API team <api@keap.com>
2025-03-19 00:12:43 +0000

    Updating SDK 0.0.31 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/ApiClient.js
+++ sdks/v2/javascript/src/keap.core.v2/ApiClient.js
@@ -16,7 +16,7 @@ import superagent from "superagent";
 
 /**
 * @module keap.core.v2/ApiClient
-* @version 0.0.30
+* @version 0.0.31
 */
 
 /**
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'core-service-v2-javascript/0.0.30'
+            'User-Agent': 'core-service-v2-javascript/0.0.31'
         };
 
         /**
