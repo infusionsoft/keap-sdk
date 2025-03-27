a48ef07f38e8321d710e35f576051d40fde42c22
Keap API team <api@keap.com>
2025-03-27 00:11:57 +0000

    Updating SDK 0.0.42 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/index.js
+++ sdks/v2/javascript/src/keap.core.v2/index.js
@@ -407,7 +407,7 @@ import TagsApi from './api/TagsApi';
 * </pre>
 * </p>
 * @module keap.core.v2/index
-* @version 0.0.41
+* @version 0.0.42
 */
 export {
     /**
--- sdks/v2/php/lib/Configuration.php
+++ sdks/v2/php/lib/Configuration.php
@@ -100,7 +100,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'core-service-v2-php/0.0.41';
+    protected $userAgent = 'core-service-v2-php/0.0.42';
 
     /**
      * Debug switch (default set to false)
@@ -433,7 +433,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 2.70.0.794081' . PHP_EOL;
-        $report .= '    SDK Package Version: 0.0.41' . PHP_EOL;
+        $report .= '    SDK Package Version: 0.0.42' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
